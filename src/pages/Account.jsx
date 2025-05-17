d3444abacb3c957166fc14a64e1718aff411b8d3
shivampandey2389 <shivampandey2389@gmail.com>
2025-05-17 15:52:54 +0530

    logout functionality

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,28 +1,49 @@
 
 import { accInfo } from '../../constant'
 import { useAuthContext } from '../context/authContext'
-
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
 const Account = () => {
-  const {account} = useAuthContext();
+  const {account,logout} = useAuthContext();
+  const handleSignOut = () =>{
+    signOut(auth).then(()=>{
+      logout();
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
   return (
     <section className='account-sec'>
       <div className="inner-sec-acc">
         <div className='acc-img'>
-          <img src={account.photoURL} alt="" height="200px" width="200px" />
+          <img src={account.photoURL} alt="profile" height="200px" width="200px" />
         </div>
         <div className='acc-info'>
           <h1>{account.displayName}</h1>
           <div className='acc-list-col'>
             {
-              accInfo.map((item)=>(
-              <div className='acc-list' key={item.id}>
-              <i className={item.iconClass}></i>
-                <div className='acc-name-other-info'>
-                <h2>{item.name}</h2>
-                <p>{item.otherInfo}</p>
-              </div>
-              </div>
-              ))
+              accInfo.map((item)=>{
+                if(item.name === "Log out"){
+                  return(
+                  <div className='acc-list' key={item.id} onClick={handleSignOut}>
+                    <i className={item.iconClass}></i>
+                    <div className='acc-name-other-info'>
+                    <h2>{item.name}</h2>
+                    <p>{item.otherInfo}</p>
+                  </div>
+                  </div>
+                  )
+                }
+                return(
+                  <div className='acc-list' key={item.id}>
+                  <i className={item.iconClass}></i>
+                    <div className='acc-name-other-info'>
+                    <h2>{item.name}</h2>
+                    <p>{item.otherInfo}</p>
+                  </div>
+                  </div>
+                )
+              })
             }
 
           </div>
